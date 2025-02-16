4fcfe6c0c3ffd85a938e509027697eb7d7b67a5d
NThuesen <nicholasnt@al.insper.edu.br>
2025-02-16 14:38:55 -0300

    tentativa 1

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,33 +1,41 @@
 #include "asf.h"
 
-int g_cnt = 0;
+volatile int g_cnt = 0;
+volatile char g_but_flag = 0;
 char g_str[10];
-int batata;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
-void btn_callback(void) {
-  printf("btn pressed \n");
 
-  if (g_cnt >= 8)
-    g_cnt = 0;
+void btn_callback(uint gpio, uint32_t events) {
+  g_but_flag = 1; 
+}
 
-  g_cnt = 2;
 
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
+void update_display(void) {
+  if (g_but_flag){
+    
+    g_but_flag = 0;
+    if (g_cnt >= 8){
+      g_cnt = 0;
+    }else{
+      g_cnt = 2;
+    }
+
+    for (i = 0; i < g_cnt; i++) {
+        g_str[i] = '*';
+      }
+    g_str[g_cnt] = '\0';
     gfx_mono_draw_string(g_str, 0, 0, &sysfont);
   }
+  
+  
 }
 
 void main(void) {
   // ...
-
-  batata = batata + 1;
+  gpio_set_irq_enabled_with_callback(BOTAO, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
   while (1) {
+    update_display(); 
   }
 }
